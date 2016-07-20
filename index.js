f56599fe0493c50ae34509d149c0b086d24a3f84
sigmoidium <sj-bautista@hotmail.com>
2016-07-20 11:19:12 +1000

    Final second

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var  app = require( 'express' )();
 var http = require( 'http' ).Server( app );
 var io = require( 'socket.io' )( http );
 
+var port = process.env.PORT || 8080;
+
 app.get( '/',function( req, res ){
   res.sendFile(__dirname + '/index.html' );
 } );
@@ -12,6 +14,6 @@ io.on( 'connection', function( socket ){
   } );
 } );
 
-http.listen( 3000, function(){
-  console.log( 'listening on *:3000' )
+http.listen( port, function(){
+  console.log( '!!!' )
 } );
